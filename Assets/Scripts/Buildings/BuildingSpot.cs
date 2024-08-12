f17598389562f180dba4b7cb257176fdfa3a0cd8
Ron Star <ronstar0502@gmail.com>
2024-08-12 19:50:49 +0300

    /

--- Assets/Scripts/Buildings/BuildingSpot.cs
+++ Assets/Scripts/Buildings/BuildingSpot.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class BuildingSpot : MonoBehaviour
 {
     [SerializeField] private GameObject building;
-
+    private bool hasBuilding;
     public void BuildAtSpot()
     {
+        if (hasBuilding) return;
         GameObject spotBuilding = Instantiate(building,transform.position,Quaternion.identity,transform);
     }
     public GameObject GetBuilding()
