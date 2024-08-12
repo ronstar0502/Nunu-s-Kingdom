6cb3733eaa99615f7c23cf5acb2dc2eae3ec97a3
Ron Star <ronstar0502@gmail.com>
2024-08-12 17:52:52 +0300

    player controller pt1

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private float _horizontalInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        InputMovement();
+    }
+    private void InputMovement()
+    {
+        if (_horizontalInput > 0)
+        {
+            rb.velocity = Vector2.right * moveSpeed * Time.fixedDeltaTime;
+            sr.flipX = true;
+        }
+        else if (_horizontalInput < 0)
+        {
+            rb.velocity = Vector2.left * moveSpeed * Time.fixedDeltaTime;
+            sr.flipX = false;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+public class PlayerData
+{
+    public float maxHealth;
+    public float health;
+    public void SetMaxHealth()
+    {
+        health = maxHealth;
+    }
+}
