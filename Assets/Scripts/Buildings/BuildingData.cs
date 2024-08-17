4803f17599a35177fed1f6beb446b61aa92f5594
Ron Star <ronstar0502@gmail.com>
2024-08-17 19:43:30 +0300

    added interfaces for interactable and damageable
    addition for the interface i implemented the interact method for building spot and building upgrade

--- Assets/Scripts/Buildings/BuildingData.cs
+++ Assets/Scripts/Buildings/BuildingData.cs
@@ -3,14 +3,31 @@ using UnityEngine;
 [System.Serializable]
 public class BuildingData 
 {
-    public Sprite buildingSprite;
     public string buildingName;
+    [Header("Building Sprites And Levels")]
+    public Sprite[] buildingSprites;
+    public int[] costLevels;
+    public int buildingLevel;
+    [Header("Building Data")]
+    public float maxBuildingHP;
     public float buildingHP;
     public float buildingTime;
-    public int cost;
 
+    public void InitBuildingHP()
+    {
+        buildingHP = maxBuildingHP;
+    }
+    public void TakeDamage(float damage)
+    {
+        buildingHP -= damage;
+    }
+
+    public int GetNextLevelCost()
+    {
+        return costLevels[buildingLevel];
+    }
     public override string ToString()
     {
-        return $"{buildingName} : HP -> {buildingHP} , Building Cost -> {cost}";
+        return $"{buildingName} : HP -> {buildingHP} , Building Cost -> {costLevels[0]}";
     }
 }
