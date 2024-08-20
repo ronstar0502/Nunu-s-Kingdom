a97b13d4f4190437f370d0108066e74dfb8c6706
Ron Star <ronstar0502@gmail.com>
2024-08-20 15:04:56 +0300

    some comments

--- Assets/Scripts/Buildings/Types/Defense/DefenseWall.cs
+++ Assets/Scripts/Buildings/Types/Defense/DefenseWall.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class DefenseWall : Building
 {
-    //regular defense wall
+    //regular defense wall - level 1
+    //medium defense wall , has more hp - level 2
+    //big defense wall , more hp , ability to but an additional deefnse upgrade on like spike to deal some damage back to the enemy
 }
--- Assets/Scripts/Buildings/Types/Defense/GuardTower.cs
+++ Assets/Scripts/Buildings/Types/Defense/GuardTower.cs
@@ -5,4 +5,7 @@ using UnityEngine;
 public class GuardTower : Building
 {
     //Guard Tower
+    //make slots for archers
+    //only when an archer is in the guard tower only than he cna shoot
+    //make the archer on the guard tower shoot at a direction and the first enemy to get hit the arrow will deal damage to
 }
