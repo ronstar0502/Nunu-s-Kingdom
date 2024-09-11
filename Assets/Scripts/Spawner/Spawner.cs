8022e201854b334e2c174902440954c22973de80
TacticalO <orzok169@gmail.com>
2024-09-12 00:36:19 +0300

    Spawner Delay and UI
    Spawner will spawn with delay.
    UI windows for Buildings and Info Windows

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -6,30 +7,29 @@ public class Spawner : MonoBehaviour
     [SerializeField] Transform[] spawnPoints;
     [SerializeField] private GameObject[] enemyDirectionIndicators; //place holder
     private Vector3 spawnPosition;
-    //private int randomSpawnPoint;
+    private int randomSpawnPoint;
     [SerializeField] private Wave[] waves;
     private int currWave = 0;
-    public void StartSpawning(int randomSpawnPoint)
+
+    public IEnumerator StartSpawning()
     {
         int amount = waves[currWave].enemiesAmount[0]; // ??
         randomSpawnPoint = Random.Range(0, 2);
         GameObject spawnerPortal = Instantiate(spawnerPortalPrefab, spawnPoints[randomSpawnPoint]);
 
         enemyDirectionIndicators[randomSpawnPoint].SetActive(true); //place holder
-        
-        for (int i = 0; i < waves[currWave].enemies.Length; i++)
+        if(currWave < waves.Length)
         {
-            for (int j = 0; j < waves[currWave].enemiesAmount[i]; j++)
+            for (int i = 0; i < waves[currWave].enemies.Length; i++)
             {
-                
-                spawnPosition = spawnPoints[randomSpawnPoint].position;
-                Instantiate(waves[currWave].enemies[i], spawnPosition, Quaternion.identity, spawnPoints[randomSpawnPoint]);
+                for (int j = 0; j < waves[currWave].enemiesAmount[i]; j++)
+                {
+
+                    spawnPosition = spawnPoints[randomSpawnPoint].position;
+                    Instantiate(waves[currWave].enemies[i], spawnPosition, Quaternion.identity, spawnPoints[randomSpawnPoint]);
+                    yield return new WaitForSeconds(2);
+                }
             }
-        }
-        if(currWave == waves.Length - 1)
-        {
-            return;
-            //win
         }
         //Destroy(spawnerPortal); //place holder
         currWave++;
